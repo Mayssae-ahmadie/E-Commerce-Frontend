0fae891d2417173ebb1b9b818d7744791e3b0500
Mayssae El Ahmadie <mayssae.ahmadie@gmail.com>
2023-12-14 12:33:56 +0200

    Footer

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -3,13 +3,15 @@ import './components/styles/style.css';
 import './components/styles/commonstyle.css';
 import AboutUs from './components/about';
 import Services from './components/services';
+import Footer from './components/footer';
+
 
 function App() {
   return (
     <div className="App">
       <AboutUs />
       <Services />
-
+      <Footer />
     </div>
   );
 }
