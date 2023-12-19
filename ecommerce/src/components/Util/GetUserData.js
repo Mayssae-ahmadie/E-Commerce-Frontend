4bc6708577c21abcd39bf4e4ed70eb3e784dbec0
Mayssae El Ahmadie <mayssae.ahmadie@gmail.com>
2023-12-19 16:11:56 +0200

    Merging master with Dashboard

--- ecommerce/src/components/Util/GetUserData.js
+++ ecommerce/src/components/Util/GetUserData.js
@@ -1,30 +1,6 @@
 import { jwtDecode } from "jwt-decode";
 
 export const getUserRole = () => {
-<<<<<<< HEAD
-    try {
-        const token = sessionStorage.getItem("authToken");
-        if (token) {
-            const decodedToken = jwtDecode(token);
-            return decodedToken.role;
-        }
-    } catch (error) {
-        console.error("Error decoding token:", error);
-    }
-    return null;
-};
-export const getUserID = () => {
-    try {
-        const token = sessionStorage.getItem("authToken");
-        if (token) {
-            const decodedToken = jwtDecode(token);
-            return decodedToken.id;
-        }
-    } catch (error) {
-        console.error("Error decoding token:", error);
-    }
-    return null;
-=======
   try {
     const token = sessionStorage.getItem("authToken");
     if (token) {
@@ -47,5 +23,4 @@ export const getUserid = () => {
     console.error("Error decoding token:", error);
   }
   return null;
->>>>>>> 6-dashboard-admin
 };
--- ecommerce/src/components/dashboard/AdminDashboard.js
+++ ecommerce/src/components/dashboard/AdminDashboard.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import "../styles/NavBar.css";
-import { Link, useNavigate } from "react-router-dom";
-const AdminDashboard = () => {
-  const [clicked, setclicked] = useState(false);
-  const handleClick = () => {
-    setclicked(!clicked);
-  };
-
-  return (
-    <div>
-      <nav className="Navbar">
-        <Link className="Pet-logo" to="/"></Link>
-        <div>
-          <ul id="N-menu" className={clicked ? "#N-menu active" : "#N-menu"}>
-            <li className="N-menu-items">
-              <h1 className="Mn-admin">Hello Admin</h1>
-            </li>
-
-            <li className="N-menu-button">
-              <button className="N-register">Sign Out</button>
-            </li>
-          </ul>
-        </div>
-
-        <div id="" onClick={handleClick}>
-          <i id="bar" className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
-        </div>
-      </nav>
-    </div>
-=======
 import UserTable from "../dashboard/UserTable";
 import OrderTable from "./OrderTable";
 import ProductTable from "./ProductTable";
@@ -43,7 +11,6 @@ const AdminDashboard = () => {
       <ProductTable />
       <OrderTable />
     </>
->>>>>>> 6-dashboard-admin
   );
 };
 
